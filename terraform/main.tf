2bc139e6de0f3b514c2ebcf6884c3f866a608371
Ubuntu <ubuntu@ip-10-0-22-11.ec2.internal>
2025-12-11 07:19:34 +0000

    apps

---
+++ terraform/main.tf
@@ -0,0 +1,69 @@
+
+# Get Default VPC and Subnets
+# Get default VPC
+data "aws_default_vpc" "default" {}
+
+# Get default subnets
+data "aws_subnets" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_default_vpc.default.id]
+  }
+}
+
+# Security Group
+resource "aws_security_group" "strapi_sg" {
+  name        = "strapi-sg"
+  description = "Allow Strapi and SSH"
+  vpc_id      = data.aws_default_vpc.default.id
+
+  ingress {
+    description = "Allow Strapi"
+    from_port   = 1337
+    to_port     = 1337
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    description = "Allow all outbound"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# EC2 Instance
+resource "aws_instance" "strapi" {
+  ami                    = var.ami_id
+  instance_type          = var.instance_type
+  subnet_id              = data.aws_subnets.default.ids[0]
+  vpc_security_group_ids = [aws_security_group.strapi_sg.id]
+  key_name               = var.key_name
+
+  user_data = templatefile("${path.module}/user_data.tpl", {
+    image_repo = var.image_repo
+    image_tag  = var.image_tag
+  })
+
+  tags = {
+    Name = "Strapi-Server"
+  }
+}
+
+#############################################
+# Output EC2 Public IP
+#############################################
+output "ec2_public_ip" {
+  value = aws_instance.strapi.public_ip
+}
+
---
+++ terraform/variable.tf
@@ -0,0 +1,26 @@
+[200~variable "aws_region" {
+  type    = string
+  default = "us-east-2"
+}
+
+variable "key_name" {
+  description = "EC2 Key pair name for SSH access (must exist in AWS)."
+  type        = string
+}
+
+variable "instance_type" {
+  type    = string
+  default = "t3.micro"
+}
+
+variable "image_repo" {
+  type    = string
+  description = "Docker image repository (e.g. user/strapi or 111.dkr.ecr.region.amazonaws.com/strapi)"
+}
+
+variable "image_tag" {
+  type    = string
+  description = "Docker image tag to deploy"
+  default = "latest"
+}
+
